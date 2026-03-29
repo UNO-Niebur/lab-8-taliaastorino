7a568d97ee62d755798a1238c946a2242dc8b056
taliaastorino <tastorino@unomaha.edu>
2026-03-29 17:55:29 -0500

    Update ProcessData.py
    try

--- ProcessData.py
+++ ProcessData.py
@@ -1,7 +1,7 @@
 #ProcessData.py
-#Name:
-#Date:
-#Assignment:
+#Name: Talia Astorino
+#Date: 03/29/2026
+#Assignment: 
 
 import random
 
@@ -12,8 +12,20 @@ def main():
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
+  for line in inFile:
+    line = line.strip()
+    parts = line.split(",")
 
+    first = parts[0]
+    last = parts[1]
+    major = parts[2]
+    year = parts[3]
 
+    userID = first[0].lower() = last.lower() + str(random.randint(10,99))
+
+    majorYear = major + "-" + year
+
+    outFile.write(first + "," + last + "," + userID + "," + majorYear + "\n")
 
   #Close files in the end to save and ensure they are not damaged.
   inFile.close()
